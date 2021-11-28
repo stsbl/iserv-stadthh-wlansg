ef0b5917a4f6e1bb6ba41ad0ed402984a9b2b20b
Felix Jacobi <Felix@jacobi-hamburg.net>
2021-11-28 22:24:43 +0100

    Fixed help text

--- translations/messages.php
+++ translations/messages.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 // host-config
 _p('host-config-description', 'RADIUS certificate ID');
 _p('host-config-description', 'RADIUS certificate password');
-_p('host-config-help-text', 'The Certificate must be placed in \\iserv\appdata\stadthh-wlansg-setup\certificates\%ID%.pfx. The %ID% must be replaced by the ID given here.');
+_p('host-config-help-text', 'The Certificate must be placed in \\iserv\appdata\stsbl-stadthh-wlansg\certificates\%ID%.pfx. The %ID% must be replaced by the ID given here.');
 _p('host-config-help-text', 'The password for the given RADIUS certificate.');
 _p('host-config-group', 'WLAN-SG (Stadt Hamburg)');
