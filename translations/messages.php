b580434d3d8de18d8e9821e724b7f8c8d1b36526
Felix Jacobi <felix.jacobi@iserv.eu>
2021-11-28 20:39:21 +0100

    Added t10ns

---
+++ translations/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+// host-config
+_p('host-config-description', 'RADIUS certificate ID');
+_p('host-config-description', 'RADIUS certificate password');
+_p('host-config-help-text', 'The Certificate must be placed in \\iserv\appdata\stadthh-wlansg-setup\certificates\%ID%.pfx. The %ID% must be replaced by the ID given here.');
+_p('host-config-help-text', 'The password for the given RADIUS certificate.');
+_p('host-config-group', 'WLAN-SG (Stadt Hamburg)');
